e8a46d578df207ea570a8737ad95059a0b3fdca0
SJ94 <SJ94@users.noreply.github.com>
2019-11-27 17:05:33 +0900

    Initial project commit

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator;
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update()
+    {
+        // get first finger on screen and check if it's the first frame of the finger touching the screen 
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            var cameraRotation = Quaternion.LookRotation(cameraBearing);
+            
+            GameObject obj = Instantiate(objectToSpawn, new Vector3(placementIndicator.transform.position.x - 0.02f, placementIndicator.transform.position.y, placementIndicator.transform.position.z), placementIndicator.transform.rotation);
+            GameObject obj1 = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            GameObject obj3 = Instantiate(objectToSpawn, new Vector3(placementIndicator.transform.position.x + 0.02f, placementIndicator.transform.position.y, placementIndicator.transform.position.z), placementIndicator.transform.rotation);
+        }
+
+    }
+}
---
+++ Assets/Scripts/TrailFollowsPath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailFollowsPath : MonoBehaviour
+{
+    public iTweenPath tweenPath;
+    public float time;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        List<Vector3> path = new List<Vector3>();
+
+        var cameraForward = Camera.current.transform.forward;
+        var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+        var cameraRotation = Quaternion.LookRotation(cameraBearing);
+
+        for (int i = 0; i < tweenPath.nodes.Count; i++)
+        {
+            path.Add(this.transform.position + (cameraRotation * tweenPath.nodes[i]));
+        }
+
+        iTween.MoveTo(gameObject, iTween.Hash("path", path.ToArray(), "easetype", iTween.EaseType.easeInOutSine, "time", time));
+    }
+}
