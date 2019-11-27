2998bf04d4c76bdc9bbf6878bdb3da017b0c7183
SJ94 <SJ94@users.noreply.github.com>
2019-11-27 17:25:02 +0900

    Update ObjectSpawner.cs
    Commenting out camera rotation variables.

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -17,9 +17,9 @@ public class ObjectSpawner : MonoBehaviour
         // get first finger on screen and check if it's the first frame of the finger touching the screen 
         if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
-            var cameraForward = Camera.current.transform.forward;
-            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
-            var cameraRotation = Quaternion.LookRotation(cameraBearing);
+            //var cameraForward = Camera.current.transform.forward;
+            //var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            //var cameraRotation = Quaternion.LookRotation(cameraBearing);
             
             GameObject obj = Instantiate(objectToSpawn, new Vector3(placementIndicator.transform.position.x - 0.02f, placementIndicator.transform.position.y, placementIndicator.transform.position.z), placementIndicator.transform.rotation);
             GameObject obj1 = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
